04b108ae9ea63689751d9fe335ce3f7ae8036ddf
Gibson <gibsonxhan@gmail.com>
2020-11-21 13:57:14 -0800

    fix optimize solution, where pointer incrementation is decided by perserving the tallest height

--- medium/containerWithMostWater.js
+++ medium/containerWithMostWater.js
@@ -2,17 +2,16 @@ let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 let height2 = [4, 3, 2, 1, 4];
 let height3 = [1, 2, 1];
 
-
 function checkWater(arr) {
   let largestArea = 0;
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       let currWater = calcArea(i, j, arr);
-      largestArea = Math.max(currWater, largestArea)
+      largestArea = Math.max(currWater, largestArea);
     }
   }
 
-  console.log('largest Area', largestArea)
+  console.log("largest Area", largestArea);
 }
 
 checkWater(height);
@@ -30,27 +29,26 @@ function checkWater2Pointer(arr) {
   //start the increment out of bound
   //bc algro stepping requires decrement
   let j = arr.length;
-  let largestArea = 0
+  let largestArea = 0;
 
   while (i < j) {
+    let currArea = calcArea(i, j, arr);
+    largestArea = Math.max(currArea, largestArea);
 
-    j--;
-    let currArea = calcArea(i, j, arr)
-    largestArea = Math.max(currArea, largestArea)
-
-    i++;
-    let currArea2 = calcArea(i, j, arr)
-    largestArea = Math.max(currArea2, largestArea)
+    //move the increment with the smallest height
+    //intuition: bigest volume comes from longer the distance, and taller the height
+    //as we reduce the search space, we want to keep the tallest possible height
+    arr[i] < arr[j] ? i++ : j++;
   }
-  console.log('largest Area v2', largestArea)
+  console.log("largest Area v2", largestArea);
 }
 
 function calcArea(i, j, arr) {
-  let minHeight = Math.min(arr[i], arr[j])
-  let dist = j - i
-  return dist * minHeight
+  let minHeight = Math.min(arr[i], arr[j]);
+  let dist = j - i;
+  return dist * minHeight;
 }
 
-checkWater2Pointer(height)
-checkWater2Pointer(height2)
-checkWater2Pointer(height3)
+checkWater2Pointer(height);
+checkWater2Pointer(height2);
+checkWater2Pointer(height3);
