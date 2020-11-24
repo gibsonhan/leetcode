4331e683b12ff0f61e851cb726126b631528c02b
Gibson Han <gibsonxhan@gmail.com>
2020-11-24 09:20:36 -0800

    add optimize solution using middleout approach
    restore bf solution and add bf + dynamic programing solution

--- medium/countPalindromicSubstring.js
+++ medium/countPalindromicSubstring.js
@@ -1,118 +1,110 @@
 /**
- *  Start from the middle
- *      2 cases to consider
- *          aa - evenFind
- *          bab - oddFind
+ *  Cache all possible combination of string
+ *  verify each one?
  *
- * let string = "babad"
+ * 	 for(let i ..)
+ * 		 for ( let j + 1 ...)
  *
- *      b
- *      ba
- *      bab
- *      baba
- *      babad
+ * 		isPalidrome
  *
- *      a
- *      ab
- *      aba
- *      abad
- *
- *      b
- *      ba
- *      bad
- *
- *      a
- *      ad
- *
- *      d
+ * 		space (O(n^2))
+ * 		time (O(n))
  */
 
-let string = "babad";
-let string1 = "racecar";
-let string2 = "qwebccbjfeifj";
+let s = "aaa";
+let s1 = "racecar";
+let s2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
-function bfLongestPalidrome(str) {
-  let longestPad = "";
+function bfCountPadSub(str) {
+  let cache = [];
 
-  //find all str combination iterative and order
   for (let i = 0; i < str.length; i++) {
-    let s = str[i];
+    let paldStr = str[i];
+    cache.push(paldStr);
     for (let j = i + 1; j < str.length; j++) {
-      s += str[j];
-      longestPad = longestStrPad(s, longestPad);
+      paldStr += str[j];
+      cache.push(paldStr);
     }
   }
-  console.log("longest palidrome string v1", longestPad);
-}
 
-function longestStrPad(str1, str2) {
-  return isPalidrome(str1) ? longestStr(str1, str2) : str2;
-
-  function isPalidrome(str) {
-    let i = 0;
-    let j = str.length - 1;
-
-    while (i < j) {
-      if (str[i] !== str[j]) return false;
-      i++;
-      j--;
+  let count = 0;
+  cache.forEach((str) => {
+    if (isValidPalidrom(str)) {
+      count++;
     }
+  });
+  console.log("what is count", count);
+}
 
-    return true;
+function isValidPalidrom(str) {
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i <= j) {
+    if (str[i] !== str[j]) return false;
+    i++;
+    j--;
   }
+  return true;
 }
 
-function longestStr(str1, str2) {
-  return str1.length > str2.length ? str1 : str2;
-}
+bfCountPadSub(s);
+bfCountPadSub(s1);
+bfCountPadSub(s2);
 
-bfLongestPalidrome(string);
-bfLongestPalidrome(string1);
-bfLongestPalidrome(string2);
-
-/**
- *  Start palidrom search from the middle
- *  bc the longest palidrome will be in the mid
- *
- *   Big O(n^2)
- *   Big Ω(n)
- *
- */
-
-function longestPalidrome(str) {
-  let len = 0;
-  let len1 = 0;
-  let len2 = 0;
-  let longestLen = 0;
-  let start = 0;
-  let end = 0;
+function bfDynamicCountPadSub(str) {
+  let cache = {}
+  let count = 0;
 
   for (let i = 0; i < str.length; i++) {
-    len1 = middleExpand(i, i, str);
-    len2 = middleExpand(i, i + 1, str);
-    len = Math.max(len1, len2);
-    if (len > longestLen) {
-      longestLen = len;
-      //update the indices of current longest palidrome
-      start = i - Math.floor((len - 1) / 2);
-      end = i + Math.floor(len / 2);
+    let s = str[i]
+    if (cache[s] === undefined) {
+      cache[s] = true
+    }
+    count += 1
+
+    for (let j = i + 1; j < str.length; j++) {
+      s += str[j]
+
+      if (cache[s] === true) {
+        count += 1
+        continue;
+      }
+      if (isValidPalidrom(s)) {
+        cache[s] = true
+        count += 1;
+      }
     }
   }
-  console.log(str.slice(start, end + 1));
+  console.log('what is count', count)
 }
 
-function middleExpand(left, right, str) {
+bfDynamicCountPadSub(s);
+bfDynamicCountPadSub(s1);
+bfDynamicCountPadSub(s2);
+
+function countPadSub(str) {
+  let count = 0
+  for (let i = 0; i < str.length; i++) {
+    count += middleOut(i, i, str)
+    count += middleOut(i, i + 1, str)
+  }
+  console.log('what is count', count)
+}
+
+function middleOut(left, right, str) {
+  let count = 0
   let l = left;
   let r = right;
 
   while (l >= 0 && r < str.length && str[l] === str[r]) {
     l--;
     r++;
+    count += 1
   }
-
-  return r - l - 1;
+  return count
 }
 
-longestPalidrome(string);
-longestPalidrome(string1);
-longestPalidrome(string2);
+countPadSub(s)
+countPadSub(s1)
+countPadSub(s2)
